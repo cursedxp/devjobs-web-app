458d062f732e1b699a3206e6fdc0c3d1502f82d6
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 13:50:19 +0100

    home route added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Router, Route } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route path="/" element={<Home />} />
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
