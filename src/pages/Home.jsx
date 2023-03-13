aa1f6849ebc24cb71b059c586ba440791b1a206c
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 15:10:48 +0100

    context added to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/context";
+
 export default function Home() {
+  const theme = useContext(ThemeContext);
+
   return <h1>Home</h1>;
 }
