18d25284420ad1936ec8b90b751b8b90b9385172
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 15:15:33 +0100

    default theme context has been added

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export function ContextWrapper(props) {
-  const [theme, setTheme] = useState("");
+  const [theme, setTheme] = useState("dark");
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {props.children}
