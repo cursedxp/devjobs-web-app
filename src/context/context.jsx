caa11c9b884a9f2b9def22cb3b7cae00f97b4b35
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 15:58:43 +0100

    handle theme change function

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -3,8 +3,18 @@ export const ThemeContext = createContext();
 
 export function ContextWrapper(props) {
   const [theme, setTheme] = useState("light");
+
+  //Changes app theme
+  const handleTheme = () => {
+    const app = document.querySelector(".App");
+    const newTheme = theme === "light" ? "dark" : "light";
+    app.classList.remove(theme);
+    app.classList.add(newTheme);
+    setTheme(newTheme);
+  };
+
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, handleTheme }}>
       {props.children}
     </ThemeContext.Provider>
   );
