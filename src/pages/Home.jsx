e3fe52f7fc5f19e879fab12a3ff762ca80662d48
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 16:09:13 +0100

    header added to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/context";
+import Header from "../components/header/Header";
 
 export default function Home() {
   const theme = useContext(ThemeContext);
 
-  return <h1>Home</h1>;
+  return (
+    <main>
+      <Header></Header>
+      <h1>Home</h1>
+    </main>
+  );
 }
