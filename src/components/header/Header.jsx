95a20f4b63743024beab7a1f1257cd75bcc3b2b5
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 14:06:11 +0100

    Header component created

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="">
+      <div>
+        <div className="brand-name"></div>
+        <div className="change-theme"></div>
+      </div>
+      <div className="filter"></div>
+    </header>
+  );
+}
