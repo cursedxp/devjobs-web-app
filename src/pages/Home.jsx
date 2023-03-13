877835be83fe78db159e793f407ef129fef2984e
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-13 17:52:17 +0100

    context removed from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react";
-import { ThemeContext } from "../context/context";
 import Header from "../components/header/Header";
 
 export default function Home() {
-  const theme = useContext(ThemeContext);
-
   return (
     <main>
       <Header></Header>
