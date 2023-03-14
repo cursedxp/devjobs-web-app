b9bbb2f862aef2d9ea0f4af7bac8b09a8124a174
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-14 10:56:18 +0100

    basic html structure of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,36 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../context/context";
+import filterIcon from "../../assets/mobile/icon-filter.svg";
+import searchIcon from "../../assets/mobile/searchIcon.svg";
+import sunIcon from "../../assets/mobile/sunIcon.svg";
+import moonIcon from "../../assets/mobile/MoonIcon.svg";
 
 export default function Header() {
   const { handleTheme } = useContext(ThemeContext);
   return (
     <header className="">
       <div>
-        <div className="brand-name"></div>
-        <div className="change-theme" onClick={handleTheme}>
-          <div className="switch">
-            <input type="checkbox" />
-            <span className="slider"></span>
+        <div className="brand-name">Devjobs</div>
+        <div>
+          <img src={sunIcon} alt="light theme" width={"20px"} height={"20px"} />
+          <div className="change-theme" onClick={handleTheme}>
+            <div className="switch">
+              <input type="checkbox" />
+              <span className="slider"></span>
+            </div>
           </div>
+          <img src={moonIcon} alt="dark theme" width={"20px"} height={"20px"} />
         </div>
       </div>
-      <div className="filter"></div>
+      <div className="filter">
+        <input type="search" />
+        <button>
+          <img src={filterIcon} alt="filter" width={"20px"} height={"20px"} />
+        </button>
+        <button>
+          <img src={searchIcon} alt="" />
+        </button>
+      </div>
     </header>
   );
 }
