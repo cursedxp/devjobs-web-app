01956450d49b666d117126eb67194e2ba25de337
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-17 11:07:25 +0100

    jobpost comp has created

---
+++ src/components/JobPost/JobPost.jsx
@@ -0,0 +1,17 @@
+export default function JobPost({ job, id }) {
+  return (
+    <div className="job-post" key={id}>
+      <img className="company-logo" src={job.logo} alt={job.company} />
+      <div>
+        <div className="postedAt">
+          {job.postedAt}
+          <span className="separator"></span>
+          {job.contract}
+        </div>
+      </div>
+      <div className="position">{job.position}</div>
+      <div className="componay-name">{job.company}</div>
+      <div className="location">{job.location}</div>
+    </div>
+  );
+}
