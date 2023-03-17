2d9ce0758572f6cbfda0eacdd9a18719af6ef180
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-17 14:25:24 +0100

    default bg class setted to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App light">
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
