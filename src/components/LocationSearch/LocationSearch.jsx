13f5e06714625d64f4c22bcba78314fef4860d3a
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-19 17:42:05 +0100

    location search component updated

--- src/components/LocationSearch/LocationSearch.jsx
+++ src/components/LocationSearch/LocationSearch.jsx
@@ -1,50 +1,72 @@
 import { useContext } from "react";
 import { useState } from "react";
 import { ThemeContext } from "../../context/context";
+import "./LocationSearch.scss";
 
-export default function LocationSearch(second) {
+export default function LocationSearch(props) {
   const { jobs, setJobList } = useContext(ThemeContext);
   const [location, setLocation] = useState("");
   const [isChecked, setIsChecked] = useState(false);
-  const handleLocationSearch = (location) => {
+
+  const handleLocationSearch = () => {
+    const locationText = location.toLowerCase();
     if (location === "") {
       setJobList(jobs);
     } else {
       //Filters job location
       const filteredLocations = jobs.filter((job) => {
         const jobLocation = job.location.toLowerCase();
-        const locationText = location.toLowerCase();
-        return jobLocation.includes(locationText);
+        return isChecked
+          ? jobLocation.includes(locationText) && job.contract === "Full Time"
+          : jobLocation.includes(locationText);
       });
       setJobList(filteredLocations);
     }
   };
 
-  const checkState = (e) => {
-    console.log(e.target.value);
-  };
-
   return (
-    <div className="location-contract">
-      <input
-        type="search"
-        name="location"
-        value={location}
-        onChange={(e) => {
-          setLocation(e.target.value);
-          handleLocationSearch(e.target.value);
-        }}
-      />
-      <label htmlFor="">
+    <div className="location-contract modal-style">
+      <div className="input-wrapper border-1">
+        <div className="icon-wrapper">
+          <img src="./assets/icons/pin.svg" alt="" />
+        </div>
         <input
-          type="checkbox"
-          name="contract"
-          checked={isChecked}
-          onChange={checkState}
+          type="search"
+          name="location"
+          placeholder="Filter by location"
+          value={location}
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
         />
-        Full time only
-      </label>
-      <button>Search</button>
+      </div>
+      <div className="input-wrapper column gap-3 border-none">
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            name="contract"
+            value={isChecked}
+            onChange={(e) => {
+              if (e.target.checked === true) {
+                setIsChecked(true);
+              } else {
+                setIsChecked(false);
+              }
+            }}
+          />
+          <label htmlFor="">Full time only</label>
+        </div>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleLocationSearch();
+            props.setShowModal(false);
+          }}
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 }
