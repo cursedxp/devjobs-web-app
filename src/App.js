98a1fda4479a040b4962b2ee77df123f817af4c4
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-20 14:13:19 +0100

    header added to app js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App light">
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={""}></Route>
       </Routes>
     </div>
   );
