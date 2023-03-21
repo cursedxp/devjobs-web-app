d4bf148bc21a610715dad1e8eb184fedc1f8cb4a
Anil Ozsoy <anilozsoy@gmail.com>
2023-03-21 10:25:03 +0100

    job details added to appjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
+import JobDetails from "./pages/JobDetails";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/:id" element={""}></Route>
+        <Route path="/jobs/:id" element={<JobDetails />}></Route>
       </Routes>
     </div>
   );
